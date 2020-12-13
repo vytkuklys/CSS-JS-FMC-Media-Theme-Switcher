4d5a4c7cf577c4f695a73c87f82e18bf231d0511
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2020-12-13 02:02:03 +0200

    some changes

---
+++ main.js
@@ -0,0 +1,22 @@
+const body = document.body;
+
+function toggleThemes() {
+    let container = document.querySelector(".container");
+    let toggleBall = document.querySelector(".toggle-ball");
+    let boxes = document.querySelectorAll("div.big-box, div.small-box");
+    let boxesHover = document.querySelector(".big-box:hover");
+    let textItems = document.querySelectorAll("h4.header_h4, p.screen-name, h4.big-box_h4, p.small-box-type_p");
+
+    console.log(boxesHover);
+    container.classList.toggle("container__light-theme");
+
+    toggleBall.classList.toggle("toggle-ball__light-theme");
+
+    boxes.forEach(function (box) {
+        box.classList.toggle("box__light-theme");
+    });
+
+    textItems.forEach(function (textItem) {
+        textItem.classList.toggle("text__light-theme");
+    });
+}
